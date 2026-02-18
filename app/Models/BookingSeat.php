7ff23b90ea7ce14efdce3f33053611b90e6dc124
majid172 <luthfullahhilmajid172@gmail.com>
2026-02-18 15:10:19 +0600

    model and migration

--- app/Models/BookingSeat.php
+++ app/Models/BookingSeat.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class BookingSeat extends Model
 {
-    //
+    protected $fillable = [
+        'booking_id',
+        'seat_id',
+        'price',
+        'status',
+        // 'seat_number' can be accessed via seat relationship
+    ];
+
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
+
+    public function seat()
+    {
+        return $this->belongsTo(Seat::class);
+    }
 }
